e9b005764db7bd20d544b8d37552b80fe954d3e2
Dina Kendle <dina.kendle@gmail.com>
2020-04-24 21:54:44 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,11 @@
 class Meal
-
+  attr_accessible :waiter, :customer, :total, :tip 
+  
+  
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+  end
 end
